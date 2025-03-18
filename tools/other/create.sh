a0a7e1a745a0baf799a83e98481ec77ee9dfe32d
Graziano Giuliani <ggiulian@ictp.it>
2025-03-18 15:39:16 +0100

    name change

--- tools/other/create.sh
+++ tools/other/create.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ifile=$1 #"/home/gsannino/MIT/MITgcm_c63s/verification/MED12_ORIGINAL/build/DUMMY.f"
+ifile=$1
 
 i1=`grep -n "INITINIT" $ifile | awk -F: '{print $1}'`
 i2=`grep -n "ENDEND" $ifile | awk -F: '{print $1}'`
